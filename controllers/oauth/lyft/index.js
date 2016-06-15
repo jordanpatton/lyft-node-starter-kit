7f0ca215398b18d38a20b8df61a1d9d76817a323
Jordan Patton <jordanpatton@gmail.com>
2016-06-14 18:28:56 -0700

    removed nedb for general purposes; only using it for session storage now

--- controllers/oauth/lyft/index.js
+++ controllers/oauth/lyft/index.js
@@ -1,15 +1,5 @@
-/* dependencies */
-var NeDB = require('nedb');
-var path = require('path');
-
 /* global configuration */
 var config = require('../../../config/config');
-
-/* database initialization */
-var databaseLyftAuthorizations = new NeDB({
-  filename: path.join(__dirname, '../../../databases/lyft/authorizations.db'),
-  autoload: true
-});
 
 
 /*================*/
@@ -17,33 +7,37 @@ var databaseLyftAuthorizations = new NeDB({
 /*================*/
 
 exports.handleAuthorization = function (req, res, next) {
-  console.log('===== handleAuthorization =====');
-  var state = Date.now();
-  databaseLyftAuthorizations.insert({
-    'state': state
-  }, function (err, doc) {
-    console.log('err', err);
-    console.log('doc', doc);
-    res.redirect(
-      config.LYFT_API_URI + '/oauth/authorize'
-      + '?client_id='     + config.LYFT_CLIENT_ID
-      + '&response_type=' + 'code'
-      + '&scope='         + 'offline%20public%20profile%20rides.read%20rides.request'
-      + '&state='         + state
-    );
-  });
+  /* generate state value */
+  var state = Date.now().toString();
+  /* store state in session */
+  req.session.state = state;
+  /* redirect with state */
+  res.redirect(
+    config.LYFT_API_URI + '/oauth/authorize'
+    + '?client_id='     + config.LYFT_CLIENT_ID
+    + '&response_type=' + 'code'
+    + '&scope='         + 'offline%20public%20profile%20rides.read%20rides.request'
+    + '&state='         + state
+  );
 };
 
 exports.handleLanding = function (req, res, next) {
-  console.log('===== handleLanding =====');
-  databaseLyftAuthorizations.insert({
-    'code': req.query.code,
-    'state': req.query.state
-  }, function (err, doc) {
-    console.log('err', err);
-    console.log('doc', doc);
+  /* compare session state to returned state */
+  if (req.session.state === req.query.state) {
+    /* state is valid; save code */
+    req.session.lyftCode = req.query.code;
+    /* NOTE:
+     * You should store the code in a secure database. Keeping the authorization code in
+     * the session alone is not particularly secure, and when it expires the user will
+     * have to go through the oauth authorization process again. You can greatly improve
+     * the user experience by storing that code securely with your own user_id
+     * implementation. This is where you should do that.
+     */
     res.redirect('/');
-  });
+  } else {
+    /* state is invalid; discard code */
+    res.redirect('/');
+  }
 };
 
 exports.handleRevocation = function (req, res, next) {
